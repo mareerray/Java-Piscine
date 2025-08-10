c872fe9495a2c91d83e3a7ce61dcbba1dac1136b
mareerray <mareerray@gmail.com>
2025-08-10 21:24:42 +0300

    Java exercises third day

---
+++ StringReplace/src/StringReplace.java
@@ -0,0 +1,10 @@
+public class StringReplace {
+    //replace characters
+    public static String replace(String s, char target, char replacement) {
+        return s.replace(target, replacement);
+    }
+    //replace substrings
+    public static String replace(String s, String target, String replacement) {
+        return s.replace(target, replacement);
+    }
+}
